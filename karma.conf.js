4ce91899686dd29e86c1c9549c49d4ae1563f2aa
Mauro Fruet <maurofruet@gmail.com>
2018-05-05 10:38:53 +0200

    edit karma config to load all typescript files

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
     frameworks: ["jasmine", "karma-typescript"],
 
     // list of files / patterns to load in the browser
-    files: ["src/**/*.spec.ts"],
+    files: ["src/**/*.ts"],
 
     karmaTypescriptConfig: {
       include: ["src/**/*.ts"]
