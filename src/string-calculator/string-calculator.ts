16915f1bc6f1cdfde2b2c2da020e027d1de746bf
Mauro Fruet <maurofruet@gmail.com>
2018-05-05 15:12:22 +0200

    add comments for the string calculator

--- src/string-calculator/string-calculator.ts
+++ src/string-calculator/string-calculator.ts
@@ -1,6 +1,16 @@
 import split from '../parser/parser';
 import checkNegativeNumbers from '../validator/validator';
 
+/**
+ * @ngdoc function
+ * @name string-calculator.add
+ * @param numbers: string
+ * @description It returns the sum of the numbers passed as parameter.
+ * The string must contain numbers that can be separated by multiple delimiters.
+ * The currently supported delimiters are '\n' and ','. You can specify an additional delimiter with the following
+ * syntax: "//[delimiter]\n[numbers...]".
+ * The presence of negative numbers will throw an Exception showing the list of negative numbers.
+ */
 export default function add(numbers: string): number {
   const numberList: number[] = split(numbers);
   let sum = 0;
@@ -13,5 +23,3 @@ export default function add(numbers: string): number {
 
   return sum;
 }
-
-
