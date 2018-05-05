2f2246acf4d13b9817fd2268ffdbcdde4eb961ec
Mauro Fruet <maurofruet@gmail.com>
2018-05-05 14:04:19 +0200

    add failing test about support for different delimiters

--- src/parser/parser.spec.ts
+++ src/parser/parser.spec.ts
@@ -52,4 +52,9 @@ describe('String parser', () => {
     expect(result).toEqual([0, 0, 0, 0]);
   });
 
+  it('supports different delimiters', () => {
+    const result = split('//\t\n0,1');
+    expect(result).toEqual([0, 1]);
+  });
+
 });
