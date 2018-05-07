3cd332d6a55e4303447e49a5c68e76984742cd07
Mauro Fruet <maurofruet@gmail.com>
2018-05-08 00:37:08 +0200

    refactor of the parser with an additional function

--- src/string-calculator/parser/parser.ts
+++ src/string-calculator/parser/parser.ts
@@ -17,33 +17,30 @@ const prefix = '//';
 export default function split(inputString: string): number[] {
   // Check whether the user has specified the optional delimiter.
   if (inputString.startsWith(prefix)) {
-    const [defaultDelimiter, numbersToSplit] = getDefaultDelimiter(inputString);
+    const stringWithoutPrefix = inputString.substr(prefix.length);
+    const defaultDelimiter = getDefaultDelimiter(stringWithoutPrefix);
 
     if (defaultDelimiter) {
       delimiters.push(defaultDelimiter);
-      inputString = numbersToSplit;
+      inputString = getNumbersWithoutPrefix(stringWithoutPrefix, defaultDelimiter);
     }
   }
 
   const regExp = `[${delimiters.join()}]`;
-  const list: string[] = inputString.split(new RegExp(regExp));
+  const numbers = inputString.split(new RegExp(regExp));
 
-  return list.map(Number);
+  return numbers.map(Number);
 }
 
 /**
- * It returns the default parameter specified by the user and the string to be split.
- * @param {string} inputString - The string to be parsed to obtain the optional delimiter specified by the user.
- * @return {string[]} An array composed of two elements: the first element is the additional delimiter that can be
- * used to split the numbers passed as parameter, while the second element is the list of numbers found in the list
- * passed as parameter.
+ * It returns the default delimiter specified by the user.
+ * @param {string} stringWithoutPrefix - The string to be parsed to obtain the optional delimiter specified by the user.
+ * @return {string} The additional delimiter that can be used to split the numbers.
  */
-function getDefaultDelimiter(inputString: string): string[] {
-  // remove the prefix to get the delimiter
-  const stringWithoutPrefix = inputString.substr(prefix.length);
+function getDefaultDelimiter(stringWithoutPrefix: string): string {
   const tokens = stringWithoutPrefix.split(newline);
 
-  // check that there are at least two lines: one for the delimiter and one for the string to be parsed
+  // Check that there are at least two lines: one for the delimiter and one for the string to be parsed
   if (tokens.length > 1) {
     const delimiter = tokens[0];
 
@@ -51,11 +48,21 @@ function getDefaultDelimiter(inputString: string): string[] {
       throw new Error(ParserExceptionMessages.MISSING_DELIMITER);
     }
 
-    // Compute the index at which the string to be split starts
-    const startIndex = prefix.length + delimiter.length + newline.length;
-    const numbersToSplit = inputString.substr(startIndex);
-    return [delimiter, numbersToSplit];
+    return delimiter;
+
   } else {
     throw new Error(ParserExceptionMessages.MISSING_DELIMITER_AND_STRING);
   }
+}
+
+/**
+ * It returns the string containing the numbers to be parsed, without the prefix and the delimiter
+ * @param {string} stringWithoutPrefix - A string containing the delimiter, a new line and the numbers to be parsed.
+ * @param {string} defaultDelimiter - The default delimiter specified by the user.
+ * @return {string} A string containing the numbers to be parsed.
+ */
+function getNumbersWithoutPrefix(stringWithoutPrefix: string, defaultDelimiter: string): string {
+  // Compute the index at which the string to be split starts
+  const startIndex = defaultDelimiter.length + newline.length;
+  return stringWithoutPrefix.substr(startIndex);
 }
--- src/string-calculator/validator/validator.ts
+++ src/string-calculator/validator/validator.ts
@@ -1,11 +1,11 @@
 import ValidatorExceptionMessages from './validator-exception-messages';
 
 /**
- * It checks whether the given array contains negative numbers. In such a case, an exception is thrown
- * showing the list of negative numbers found in the list.
+ * It checks whether the given array contains negative numbers. In such a case, an exception is thrown showing the
+ * list of negative numbers found in the list.
  * @param {number[]} numbers - The array of numbers to be checked.
  */
-export default function checkNegativeNumbers(numbers: number[]) {
+export default function checkNegativeNumbers(numbers: number[]): void {
   const negativeNumbers = numbers.filter((number) => number < 0 );
 
   if (negativeNumbers.length > 0) {
